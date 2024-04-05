b6015819ad72696f673e646069f4be341008d8fe
Emil Sukiennik <ewilemil@gmail.com>
2024-04-05 15:29:56 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,35 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const list = document.createElement("ul");
+    if (element) element.append(list);
+    else {
+        list.id = "tree";
+        document.body.prepend(list);
+    }
+    for(const key in data){
+        const item = document.createElement("li");
+        item.textContent = key;
+        list.append(item);
+        if (Object.keys(data[key]).length) createTree(item, data[key]);
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
